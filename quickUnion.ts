c871bdd086b538a032b02ddd273e30dcafcffe77
Shawn Timothy Ike Barza <92981591+Pistachio21@users.noreply.github.com>
2024-01-25 01:10:06 +0800

    added WeightedQuickUnion algorithm

--- quickUnion.ts
+++ quickUnion.ts
@@ -1,18 +1,21 @@
-export class QuickUnion {
+export class WeightedQuickUnion {
     id: number[]
+    sz: number[]
 
     constructor() {
         this.id = []
+        this.sz = []
     }
 
     public quickUnion(n: number) {
         for (let i = 0; i < n; i++) {
             this.id.push(i)
+            this.sz.push(1)
         }
     }
 
     public root(i: number): number {
-        while (i != this.id[i]) {
+        while (i !== this.id[i]) {
             i = this.id[i]
         }
         return i
@@ -29,11 +32,22 @@ export class QuickUnion {
     public union(s: number, v: number) {
         let i: number = this.root(s)
         let j = this.root(v)
-        this.id[i] = j
+
+        if (i === j) return;
+
+        if (this.sz[i] < this.sz[j]) {
+            this.id[i] = j;
+            this.sz[j] += this.sz[i];
+        }
+        else {
+            this.id[j] = i;
+            this.sz[i] += this.sz[j];
+        }
+
     }
 }
 
-let quickUnion = new QuickUnion();
+let quickUnion = new WeightedQuickUnion();
 quickUnion.quickUnion(50)
 quickUnion.union(43, 45)
 console.log(quickUnion.connected(43, 45))
